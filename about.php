74e86d4fbbc8920f5d6c60340f16a4e10bee338b
aniket3210 <78550841+aniket3210@users.noreply.github.com>
2022-02-21 22:20:51 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,39 @@
+<html>
+
+	<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+	<link rel="stylesheet" type="text/css" href="sty1.css">
+   
+</head>
+<body>
+    <header>
+        <div class="topnav">
+		
+           <div class="topnav">
+			<a href="http://localhost/xampp/Host/index.html" target=_blank>Home</a>
+			<a href="http://localhost/xampp/Host/about.php" target=_blank>About</a>
+			<a href="https://www.linkedin.com/in/niraj-patil-9ba817210/" target=_blank>Contact</a>
+		</div>
+        </div>
+    </header>
+	
+	
+	<div class="about">
+	<div class="inner-section">
+	 <h1>About Us</h1>
+            <p class="text">
+                Orphans need all the love and understanding they can get because they have suffered much at a very young age. 
+				They also usually have to work harder than most children for the things they want and need because they have no one to provide them with these things.
+				Hence, a charity for orphans can make all the difference in the world for these children.
+				So make sure that you know how to help these unfortunate children at least digitally with the help of the Orphan Children Charity. 
+            </p>
+            
+      </div>
+	 </div>
+  
+	
+</body>
+</html>
